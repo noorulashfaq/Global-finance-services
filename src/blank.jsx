1f6dabf595aee900e5235f225d69a5c7ee21bd15
vijayaprakash1709 <vijayaprakash885@gmail.com>
2023-10-06 18:50:42 +0530

    Some changes in blank

--- src/blank.jsx
+++ src/blank.jsx
@@ -2,12 +2,7 @@ const Vijay=()=>
 {return(
     <>
     <h1 style={{color:'red'}}>Login</h1>
-    <label>
-        <tr>
-            name of the student<
-            <input type='text'/>
-        </tr>
-    </label>
+   
     
      </>   
     )
