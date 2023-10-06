a4445fc0f095d299fba81845deadea9454c26832
vijayaprakash1709 <vijayaprakash885@gmail.com>
2023-10-06 12:48:07 +0530

    React Basics

---
+++ src/blank.jsx
@@ -0,0 +1,15 @@
+const Vijay=()=>
+{return(
+    <>
+    <h1>Hello Vijay Welcome to the page !!!!!!</h1>
+    <p>The React is Runing</p>
+     </>   
+    )
+}
+
+const Thinesh=()=>{
+    return(
+        <h1>Hello thinesh</h1>
+    )
+}
+export Vijay
