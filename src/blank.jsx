46c209c5aee73eb67f10a9fba514a130ff74f7d0
vijayaprakash1709 <vijayaprakash885@gmail.com>
2023-10-06 19:19:53 +0530

    Login form added

--- src/blank.jsx
+++ src/blank.jsx
@@ -1,8 +1,37 @@
+
 const Vijay=()=>
 {return(
     <>
-    <h1 style={{color:'red'}}>Login</h1>
-   
+   <html>
+<head>
+    <title>Login</title>
+    
+</head>
+<body>
+    <div class="Wrapper">
+        <h2>Login</h2>
+        <form class="login-form" action="login_process.php" method="POST">
+
+	
+            <div class="input-box">
+                <input type="email" name="email" required placeholder="Email"/>
+                
+            </div>
+            <div class="input-box">
+                <input type="password" name="password" required placeholder="Password"/>
+            </div>
+            <div class="remember-forget">
+                <label>
+                    <input type="checkbox"/> Remember me
+                </label>
+               
+            </div>
+            <input type="submit" class="btn" value="Login"/>
+            
+        </form>
+    </div>
+</body>
+</html>
     
      </>   
     )
